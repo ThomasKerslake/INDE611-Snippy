93cf5b91a564cf42df0cb10652771e016d850f26
ThomasKerslake <33869524+ThomasKerslake@users.noreply.github.com>
2020-03-28 15:59:38 +0000

    Updated progress on Snippy extension
    - Added write to file function from user selection.
    - Added write to file snippet formating to save code chunk correctly.
    - Added file creation / write if no snippet file is present.
    - Added right click context menu for quick snippet saving.
    - Added key binding for snippet saving.

--- extension.js
+++ extension.js
@@ -1,27 +1,98 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
 
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "Snippy" is now active!');
+	console.log('Congratulations, your extension "save-a-snip" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('extension.saveToSnippy', function() {
+		let editor = vscode.window.activeTextEditor;
+		let document = editor.document;
+		let selection = editor.selection;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		// Get the word within the selection
+		let word = document.getText(selection);
+
+		var userName = process.env['USERPROFILE'].split(path.sep)[2];
+		var userId = path.join('C:\\Users\\', userName);
+
+		const folderPath = userId + '\\AppData\\Roaming\\Code\\User\\snippets';
+
+		/**
+		 * @param {string} codeSnippet
+		 * @param {string} prefixTab
+		 * @param {string} description
+		 */
+		function renderSnippet(codeSnippet, prefixTab, description) {
+			const separatedSnippet = codeSnippet
+				.toString()
+				.replace(/\t/g, '')
+				.replace(/^\s+|\s+$/g, '')
+				.replace(/\\/g, '\\\\')
+				.replace(/"/g, '\\"')
+				.replace(/\$/g, '\\\\$')
+				.split('\r\n');
+			const separatedSnippetLength = separatedSnippet.length;
+
+			// add quotes around each line - Stop at final line and end with ','
+			const /**
+				 * @param {any} line
+				 * @param {number} index
+				 */
+			newSnippet = separatedSnippet.map((line, index) => {
+				return index === separatedSnippetLength - 1 ? `"${line}"` : `"${line}",`;
+			});
+
+			// prettier-ignore
+			return`
+			  "${description}": {
+				"prefix": "${prefixTab}",
+				"body": [
+				  ${newSnippet.join('\n')}
+				],
+				"description": "${description}"
+			  },
+			`;
+		}
+
+		fs.access(path.join(folderPath, 'save-a-snip.code-snippets'), (error) => {
+			if (!error) {
+				var snipPositon = 2;
+				var snipPath = path.join(folderPath, 'save-a-snip.code-snippets');
+				var appendSnip = renderSnippet(word, 'word', 'word2');
+
+				fs.readFile(snipPath, function newSnipp(err, data) {
+					if (err) {
+						throw err;
+					}
+					var snipFileContent = data.toString();
+					snipFileContent = snipFileContent.substring(snipPositon);
+					var snipFile = fs.openSync(snipPath, 'r+');
+					var bufferedText = new Buffer(appendSnip + snipFileContent);
+					fs.writeSync(snipFile, bufferedText, 0, bufferedText.length, snipPositon);
+				});
+
+				vscode.window.showInformationMessage('Saved your Snipp!');
+			} else {
+				fs.writeFile(
+					path.join(folderPath, 'save-a-snip.code-snippets'),
+					renderSnippet(word, 'word', 'word2'),
+					(err) => {
+						if (err) {
+							console.error(err);
+							return vscode.window.showErrorMessage('Failed to create snipFile.');
+						}
+						vscode.window.showInformationMessage('Created new snipFile and added your code snip!');
+					}
+				);
+			}
+		});
 	});
 
 	context.subscriptions.push(disposable);
@@ -34,4 +105,4 @@ function deactivate() {}
 module.exports = {
 	activate,
 	deactivate
-}
+};
